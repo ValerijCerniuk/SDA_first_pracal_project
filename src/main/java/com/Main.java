5b76c15e03223949cacf3b573510e3303f38ed05
valerij <ptrank@gmail.com>
2022-04-23 09:52:33 +0300

    Representation

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -5,8 +5,6 @@ import com.model.Flower;
 import com.primaryData.PrimaryData;
 import com.repository.Repository;
 
-import static com.repository.SQLQueries.SELECT_ALL_FLOWERS;
-import static com.repository.SQLQueries.SELECT_ALL_FLOWER_BY_ID;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,11 +12,10 @@ public class Main {
         new PrimaryData().loadPrimaryData();
 
         Repository repository = new Repository();
-        repository.findAll(SELECT_ALL_FLOWERS, Flower.class)
-                .stream().forEach(System.out::println);
+        repository.findAll( Flower.class).forEach(System.out::println);
 
         System.out.println();
-        System.out.println(repository.findById(SELECT_ALL_FLOWER_BY_ID, Flower.class, 1));
+        System.out.println(repository.findById(Flower.class, 1));
 
     }
 }
--- src/main/java/com/util/HibernateUtil.java
+++ src/main/java/com/util/HibernateUtil.java
@@ -23,7 +23,7 @@ public class HibernateUtil {
                 properties.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
                 properties.put(Environment.URL, "jdbc:mysql://localhost:3306/flower_shop?serverTimezone=UTC");
                 properties.put(Environment.USER, "root");
-                properties.put(Environment.PASS, "admin");
+                properties.put(Environment.PASS, "password");
                 properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
                 properties.put(Environment.SHOW_SQL, "true");
                 properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
